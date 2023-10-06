66da02292508b7e6c1de014221fb27756e03bace
Alexis1663 <92861778+Alexis1663@users.noreply.github.com>
2023-10-06 11:17:07 +0200

    :truck: build: add routing from home page

--- src/app/auth/services/firebase.service.ts
+++ src/app/auth/services/firebase.service.ts
@@ -1,7 +1,12 @@
 import { EventEmitter, Injectable } from '@angular/core'
 import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
 
 @Injectable({
     providedIn: 'root',
 })
-export class FireBaseService { }
+export class FireBaseService {
+    private _firebaseConfig = {
+
+    }
+}
