b284cc8480ac3c107052dd3f0ad80933771a0e9f
Theo DUPIN <Theo.DUPIN@etu.uca.fr>
2023-10-29 10:52:53 +0100

    :sparkles: add: Redirection to Home, Login and Register pages

--- src/app/shared/component/header/header.component.ts
+++ src/app/shared/component/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
     selector: 'app-header',
@@ -10,5 +11,37 @@ export class HeaderComponent {
     // url for the logo
     public imageLogo: string = '../../../../assets/images/answerz-logo.png';
 
-    constructor() { }
+    constructor(
+        private readonly router: Router,
+    ) { }
+
+    /**
+     * @author @thdupin2
+     * @date 29/10/2023
+     * @description Navigation to the Home page.
+     * @memberof HeaderComponent
+     */
+    redirectToHomePage(): void {
+        this.router.navigate(['/']);
+    }
+
+    /**
+     * @author @thdupin2
+     * @date 29/10/2023
+     * @description Navigation to the Login page.
+     * @memberof HeaderComponent
+     */
+    redirectToLoginPage(): void {
+        this.router.navigate(['/auth/login']);
+    }
+
+    /**
+     * @author @thdupin2
+     * @date 29/10/2023
+     * @description Navigation to the Register page.
+     * @memberof HeaderComponent
+     */
+    redirectToRegisterPage(): void {
+        this.router.navigate(['/auth/register']);
+    }
 }
