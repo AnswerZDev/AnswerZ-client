a4eff735aed9b4cceba302507ed9ba17668a5473
Theo DUPIN <Theo.DUPIN@etu.uca.fr>
2023-10-30 18:53:27 +0100

    :sparkles: add: Footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { NgClass, NgIf } from '@angular/common'
 import { ButtonModule } from 'primeng/button';
 import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
     imports: [
@@ -14,10 +15,12 @@ import { HeaderComponent } from './component/header/header.component';
     ],
     declarations: [
         HeaderComponent,
+        FooterComponent,
     ],
     providers: [],
     exports: [
         HeaderComponent,
+        FooterComponent,
     ]
 })
 export class SharedModule { }
