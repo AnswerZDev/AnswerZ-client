9f0648be3901750bfb115ddd7763b5729ec78244
Audric-S <sabatieraudric@gmail.com>
2024-01-11 00:08:49 +0100

    :hammer: add auto MEP on main branch push (Docker WebServer on OVHCloud host)

---
+++ .github/workflows/scripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd ~/frontend/AnswerZ-client
+git pull origin main
+npm install
+ng build
+cp -r dist/* ~/prod
+
+cp .github/workflows/scripts/Dockerfile ~/prod
+cp .github/workflows/scripts/default.conf ~/prod
+
+cd ~/prod
+
+
+container_name="answerz-front"
+
+if docker ps -q --filter "name=${container_name}" 2>/dev/null; then
+    docker stop ${container_name}
+    docker rm ${container_name}
+fi
+
+docker build --tag angular-app .
+docker run --name $container_name -p 4200:4200 -d angular-app
