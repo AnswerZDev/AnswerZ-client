5bbb0df9a916c5be3b4bd30b691021bf15ddb039
Alexis1663 <92861778+Alexis1663@users.noreply.github.com>
2024-01-12 12:05:20 +0100

    wip

---
+++ src/app/core/http/authentication/authentication.api.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing'
+import { HttpClientModule } from '@angular/common/http'
+import { AuthenticationApi } from '@api/authentication/authentication.api'
+
+describe('AuthService', () => {
+    let service: AuthenticationApi
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientModule],
+            providers: [AuthenticationApi],
+        })
+        service = TestBed.inject(AuthenticationApi)
+    })
+    it('should create an instance', () => {
+        expect(service).toBeTruthy()
+    })
+})
