2e77cfc3642abd402afdb7b8c7a3f64f8b099abb
Destroyeur6315 <romain.fillot630@gmail.com>
2024-01-24 18:46:54 +0100

    :sparkles: generate component for edit profil user

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Router, RouterModule, Routes } from "@angular/router";
 import { UserProfileComponent } from "./component/user-profile/user-profile.component";
+import { EditProfileComponent } from "./component/edit-profile/edit-profile.component";
 
 const routes: Routes = [
     {
@@ -11,6 +12,10 @@ const routes: Routes = [
     {
         path: 'profile',
         component: UserProfileComponent,
+    },
+    {
+        path: 'profile/edit',
+        component: EditProfileComponent
     }
 ]
 
