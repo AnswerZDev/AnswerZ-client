f531de1a7b8f0c7fd31d32f54c534e3b23df7807
HugoooR <hugo.rochelle@gmail.com>
2024-01-24 17:56:43 +0100

    fix header

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { NgClass, NgIf } from '@angular/common'
 import { HomeRoutingModule } from './home-routing.module'
 import { IndexComponent } from './component/index/index.component';
-import { HeaderComponent } from './component/header/header.component';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { SharedModule } from "../shared/shared.module";
@@ -21,11 +20,9 @@ import { SharedModule } from "../shared/shared.module";
     ],
     declarations: [
         IndexComponent,
-        HeaderComponent,
     ],
     providers: [],
     exports: [
-        HeaderComponent,
         ButtonModule,
     ]
 })
