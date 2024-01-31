f8856c6a368eb6f88b09eca5eda9c1c91ed5bc17
HugoooR <hugo.rochelle@gmail.com>
2024-01-31 15:29:43 +0100

    :construction: continue forgot password page, just need to fix some little style

--- src/app/auth/pages/forgot-password/forgot-password.component.ts
+++ src/app/auth/pages/forgot-password/forgot-password.component.ts
@@ -6,7 +6,20 @@ import { Router } from '@angular/router';
     templateUrl: './forgot-password.component.html',
     styleUrls: ['./forgot-password.component.scss'],
 })
+
 export class ForgotPasswordComponent {
     constructor(private readonly router: Router) { }
+
+    /**
+     * @author @HugoooR
+     * @date 31/01/2024
+     * @description Navigation to the Login page.
+     * @memberof HomePage
+     */
+    redirectToLoginPage(): void {
+        this.router.navigate(['/auth/login']);
+    }
+
+    public email = "Email";
 
 }
