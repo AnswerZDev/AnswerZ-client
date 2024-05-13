fc48b1f38f8c50106157bef0d797ab68e77ab373
Alexis1663 <92861778+Alexis1663@users.noreply.github.com>
2024-05-13 15:55:12 +0200

    :construction: wip: profile picture

--- src/app/user/component/user-profile/user-profile.component.ts
+++ src/app/user/component/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {SecurityService} from "../../../shared/services/security.services";
 
 @Component({
   selector: 'app-user-profile',
@@ -8,53 +9,14 @@ import { Router } from '@angular/router';
 })
 export class UserProfileComponent {
 
-  private allowedExtensions: string[] = ['jpg', 'jpeg', 'png', 'gif'];
-
-  public profil_picture: string = '../../../../assets/images/profil_user.png';
-  public import_background: string = '../../../../assets/images/import_user2.svg';
-  public picture_friend: string = '../../../../assets/images/profil.jpeg';
-  public data!: number;
-  public picture_friend2: string = '../../../../assets/images/carreau.jpeg';
-  public picture_friend3: string = '../../../../assets/images/hugo.jpeg';
-  public picture_friend4: string = '../../../../assets/images/audric.jpeg';
-  public previewImage: string="";
-
-  public backgroundImageUrl: string = "../../../../assets/images/audric.jpeg";
-
   constructor(
     private readonly router: Router,
+    private readonly _securityService: SecurityService,
   ){
   }
 
-  redirectToModifPage(): void {
-    this.router.navigate(['/']);
+  public get profilePicture(): string | undefined{
+    if(!this._securityService.user) return undefined;
+    return this._securityService.user.pictureProfileUrl;
   }
-
-  onFileSelected(event: any): void {
-    const file: File = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      const fileExtension: string = this.getFileExtension(file.name);
-
-      if(this.isExtensionAllowed(fileExtension, this.allowedExtensions)){
-        this.getFileExtension(file.name)
-      } else {
-        //  TODO() => à refaire(toast primeng !)
-      }
-      reader.onload = (e) => {
-        this.previewImage = e.target?.result as string;
-      };
-      reader.readAsDataURL(file);
-    }
-  }
-
-  private isExtensionAllowed(filename: string, allowedExtensions: string[]): boolean {
-    const fileExtension: string = this.getFileExtension(filename).toLowerCase();
-    return allowedExtensions.includes(fileExtension);
-  }
-  
-  private getFileExtension(filename: string): string {
-    return filename.split('.').pop() || '';
-  }
-
 }
